59e2a26dddc6ad57de6e304524e87fc6eab9e0b6
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-27 00:31:50 +0700

    create task.json and do test on test4

--- Testing/test4.cpp
+++ Testing/test4.cpp
@@ -6,6 +6,38 @@
 #include<algorithm>
 
 using namespace std;
+
+class ListNode {
+public: 
+    int val;
+    ListNode* next;
+
+    ListNode() {
+        val = 0;
+        next = nullptr;
+    }
+
+    ListNode(int val) {
+        this->val = val;
+        next = nullptr;
+    }
+};
+
+ListNode* createList(ListNode* head, int arr[], int n) {
+    if (n == 0) return nullptr;
+
+    head = new ListNode(arr[n - 1]);
+
+    for (int i = n - 2; i >= 0; i--) {
+        ListNode* temp = new ListNode(arr[i]);
+
+        temp->next = head;
+        head = temp;
+    }
+
+    return head;
+}
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -38,13 +70,33 @@ public:
         return head;
     }
 };
+
+void printList(ListNode* p) {
+    while (p != nullptr) {
+        cout << p->val << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
 int main() {  
-    char a = 'a';
+    ListNode* head = nullptr;
 
+    int n;
+    cin >> n;
+    int arr[n];
 
-    cout << sizeof(a) << endl;
-    cout << sizeof(a - '0') << endl;
-    // cout << obj.reverse(a);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    head = createList(head, arr, n);
+
+    Solution obj;
+
+    obj.deleteDuplicates(head);
+
+    printList(head);
 
     return 0;
 }
