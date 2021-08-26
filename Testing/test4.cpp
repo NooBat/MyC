850a94459b246e47f855d98df0c2826bff0d2bc1
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-27 00:21:38 +0700

    commit by vscode

--- Testing/test4.cpp
+++ Testing/test4.cpp
@@ -6,7 +6,38 @@
 #include<algorithm>
 
 using namespace std;
-
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        
+        ListNode* i = head;
+        ListNode* j = head->next;
+        bool removable = false;
+        
+        while (i != nullptr && j != nullptr) {
+            if (i->val == j->val) {
+                j = j->next;
+                removable = true;
+            }
+            else if (i->val != j->val || j == nullptr) {
+                ListNode* p = i->next;
+                while (p != j) {
+                    ListNode* next = p->next;
+                    p->next = nullptr;
+                    delete p;
+                    p = next;
+                }
+                
+                i->next = j;
+                i = i->next;
+                j = i->next;
+            }
+        }
+        
+        return head;
+    }
+};
 int main() {  
     char a = 'a';
 
