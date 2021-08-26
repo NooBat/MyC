05a1574d8debc7f5ae036775cf37590c1d3fba72
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-26 18:05:57 +0700

    commit by vscode

--- Testing/test4.cpp
+++ Testing/test4.cpp
@@ -7,44 +7,12 @@
 
 using namespace std;
 
-class Solution {
-private:
-    int getSmallestSize(vector<string>& strs) {
-        int min = strs[0].length();
-        for (int i = 1; i < strs.size(); i++) {
-            if (strs[i].length() < min) {
-                min = strs[i].length();
-            }
-        }
-        
-        return min;
-    }
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string result;
-        int n = getSmallestSize(strs);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < strs.size() - 1; j++) {
-                if (strs[j][i] != strs[j + 1][i]) {
-                    if (result.length() < 2)
-                    return "";
-                }
-            }
-            result += strs[0][i];
-        }
-        
-        return result;
-    }
-};
-
 int main() {  
-    vector<string> strs;
-    strs.push_back("flower");
-    strs.push_back("flow");
-    strs.push_back("flight");
-    Solution obj;
+    char a = 'a';
 
-    cout << obj.longestCommonPrefix(strs);
+
+    cout << sizeof(a) << endl;
+    cout << sizeof(a - '0') << endl;
     // cout << obj.reverse(a);
 
     return 0;
