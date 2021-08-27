aa9ea3d7ed1a1d5628e0f73eaca5e64a013ad56f
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-27 19:38:50 +0700

    commit by vscode

--- Testing/test4.cpp
+++ Testing/test4.cpp
@@ -1,95 +1,161 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<queue>
 #include<sstream>
 #include<cmath>
 #include<algorithm>
 
 using namespace std;
 
-class ListNode {
+class TreeNode {
 public: 
     int val;
-    ListNode* next;
+    TreeNode* left;
+    TreeNode* right;
 
-    ListNode() {
+    TreeNode() {
         val = 0;
-        next = nullptr;
+        left = right = nullptr;
     }
 
-    ListNode(int val) {
+    TreeNode(int val) {
         this->val = val;
-        next = nullptr;
+        left = right = nullptr;
     }
 };
 
-ListNode* createList(ListNode* head, int arr[], int n) {
-    if (n == 0) return nullptr;
-
-    head = new ListNode(arr[n - 1]);
-
-    for (int i = n - 2; i >= 0; i--) {
-        ListNode* temp = new ListNode(arr[i]);
-
-        temp->next = head;
-        head = temp;
+TreeNode *createTree(string str) {
+    if (str.length() == 0 || str[0] == 'N') {
+        return NULL;
     }
 
-    return head;
+    vector<string> v;
+
+    istringstream iss(str);
+    for (string str; iss >> str; ) {
+        v.push_back(str);
+    }
+
+    TreeNode *root = new TreeNode(stoi(v[0]));
+
+    int i = 1;
+
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty() && i < v.size()) {
+        TreeNode *curr = q.front();
+        q.pop();
+
+        string currVal = v[i];
+
+        //Implementing left child
+        if (currVal != "N") {
+            curr->left = new TreeNode(stoi(currVal));
+            q.push(curr->left);
+        }
+
+        ++i;
+
+        if (i >= v.size()) break;
+        
+        currVal = v[i];
+
+        //Implementing right child
+        if (currVal != "N") {
+            curr->right = new TreeNode(stoi(currVal));
+            q.push(curr->right);
+        }
+
+        ++i;
+    }
+
+    return root;
 }
 
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
+    void sumNode(TreeNode* root) {
+        if (root == nullptr) return;
+        if (root->left == nullptr && root->right == nullptr) return;
         
-        ListNode* i = head;
-        ListNode* j = head->next;
-        
-        while (i != nullptr || j != nullptr) {
-            if (i->val == j->val) {
-                j = j->next;
-            }
-            else if (i->val != j->val || j == nullptr) {
-                ListNode* p = i->next;
-                while (p != j) {
-                    ListNode* next = p->next;
-                    p->next = nullptr;
-                    delete p;
-                    p = next;
-                }
-                
-                i->next = j;
-                i = i->next;
-                j = i->next;
-            }
+        if (root->left == nullptr) {
+            root->right->val += root->val;
+            sumNode(root->right);
+            return;
+        }
+        if (root->right == nullptr) {
+            root->left->val += root->val;
+            sumNode(root->left);
+            return;
         }
         
-        return head;
+        root->right->val += root->val;
+        root->left->val += root->val;
+        sumNode(root->right);
+        sumNode(root->left);
+    }
+    void traverse(TreeNode* root, int targetSum, bool& result) {
+        if (root == nullptr) return;
+        
+        traverse(root->left, targetSum, result);
+        if (root->left == nullptr && root->right == nullptr) {
+            if (root->val == targetSum) result = true;
+            return;
+        }
+        traverse(root->right, targetSum, result);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+        sumNode(root);
+        
+        bool result;
+        traverse(root, targetSum, result);
+
+        return result;
     }
 };
 
-void printList(ListNode* p) {
-    while (p != nullptr) {
-        cout << p->val << " ";
-        p = p->next;
+void print(TreeNode *root) {
+    if (root == NULL) {
+        cout << "Tree is empty!\n";
+        return;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode *curr = q.front();
+        q.pop();
+
+        if (curr == NULL) {
+            cout << "N ";
+        }
+        else {
+            cout << curr->val << " ";
+            q.push(curr->left);
+            q.push(curr->right);
+        }
     }
     cout << endl;
 }
 
 int main() {  
-    ListNode* head = nullptr;
+    TreeNode* root = nullptr;
+    int a = 22;
 
-    int n = 5;
-    int arr[] = {1, 1, 2, 3, 3};
+    string str = "5 4 8 11 N 13 4 7 2 N N N 1";
 
-    head = createList(head, arr, n);
+    root = createTree(str);
+
+    print(root);
 
     Solution obj;
 
-    obj.deleteDuplicates(head);
+    cout << obj.hasPathSum(root, a) << endl;
 
-    printList(head);
-
+    print(root);
     return 0;
 }
