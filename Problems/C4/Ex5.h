f2f581bc4a4a115eb6b2c4bd5654c0b91ea4ac16
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-28 13:29:48 +0700

    create Ex5 (Doubly Linked Bag)

---
+++ Problems/C4/Ex5.h
@@ -0,0 +1,13 @@
+#ifndef _DOUBLY_LINKED_BAG
+#define _DOUBLY_LINKED_BAG
+
+#include "/Users/danielnguyen/Repo/C++/ADT/Doubly_Node.h"
+#include "/Users/danielnguyen/Repo/C++/ADT/BagInterface.h"
+
+using namespace std;
+
+template<class ItemType>
+class DoublyLinkedBag : public BagInterface<ItemType> {
+
+};
+#endif
