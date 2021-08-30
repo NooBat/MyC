46499ebe2da310fb9ab8912c88070162dfce842b
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-31 00:55:45 +0700

    symbol fĩ

--- Assignment1-2/SymbolTable.cpp
+++ Assignment1-2/SymbolTable.cpp
@@ -1,5 +1,39 @@
 #include "SymbolTable.h"
 
+Node* SymbolTable::getPtrTo(const string& target_variable) const {
+    if (headPtr == nullptr) return nullptr;
+
+    Node* p = headPtr;
+
+    while (p != nullptr) {
+        if (p->getVariable() == target_variable) return p;
+        p = p->getNext();
+    }
+
+    return nullptr;
+}
+
+vector<string> SymbolTable::tokenize(string str, string del = " ") const {
+    vector<string> result;
+    int start = 0;
+    int end = (int)str.find(del);
+
+    while (end != -1) {
+        result.push_back(str.substr(start, end - start));
+        start = (int)end + 1;
+        int temp = end;
+        end = (int)str.find(del, start);
+        if (end == temp + 1) throw InvalidInstruction(str);
+    }
+
+    return result;
+}
+
 void SymbolTable::run(string filename) {
+    ifstream myfile(filename); 
+    if (myfile.is_open()){
+        string tmp;
+
+    }
     cout << "success";
 }
--- Assignment1-2/SymbolTable.h
+++ Assignment1-2/SymbolTable.h
@@ -2,7 +2,56 @@
 #define SYMBOLTABLE_H
 #include "main.h"
 
+class Node {
+private:
+    string variable;
+    string item;
+    Node* next;
+
+public:
+    Node() {
+        this->setNext(nullptr);
+    }
+
+    Node(const string& variable, const string& item) {
+        this->setVariable(variable);
+        this->setItem(item);
+        this->setNext(nullptr);
+    }
+
+    void setVariable(const string& variable) {
+        this->variable = variable;
+    }
+
+    void setItem(const string& item) {
+        this->item = item;
+    }
+
+    void setNext(Node* next) {
+        this->next = nullptr;
+    }
+
+    string getVariable() const {
+        return this->variable;
+    }
+
+    string getItem() const {
+        return this->item;
+    }
+
+    Node* getNext() const {
+        return this->next;
+    }
+};
+
+
+
 class SymbolTable {
+private:
+    Node* headPtr;
+
+    Node* getPtrTo(const string& target_variable) const;
+    vector<string> tokenize(string str, string del = " ") const;
 public:
     SymbolTable() {}
     void run(string filename);
