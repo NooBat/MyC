7a8c5e0dffd5164a589d3ec19985e3b68bd726be
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2021-08-31 13:26:10 +0700

    testing preToInfix

--- Testing/test4.cpp
+++ Testing/test4.cpp
@@ -1,161 +1,66 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<stack>
 #include<queue>
 #include<sstream>
 #include<cmath>
 #include<algorithm>
 
 using namespace std;
-
-class TreeNode {
-public: 
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-
-    TreeNode() {
-        val = 0;
-        left = right = nullptr;
+ 
+// function to check if character is operator or not
+bool isOperator(char x) {
+  switch (x) {
+  case '+':
+  case '-':
+  case '/':
+  case '*':
+    return true;
+  }
+  return false;
+}
+ 
+// Convert prefix to Infix expression
+string preToInfix(string pre_exp) {
+  stack<string> s;
+ 
+  // length of expression
+ 
+  // reading from right to left
+  for (int i = pre_exp.length() - 1; i >= 0; i--) {
+ 
+    // check if symbol is operator
+    if (isOperator(pre_exp[i])) {
+ 
+      // pop two operands from stack
+      string op1 = (string)s.top();   s.pop();
+      string op2 = (string)s.top();   s.pop();
+ 
+      // concat the operands and operator
+      string temp = "(" + op1 + pre_exp[i] + op2 + ")";
+ 
+      // Push string temp back to stack
+      s.push(temp);
     }
-
-    TreeNode(int val) {
-        this->val = val;
-        left = right = nullptr;
+ 
+    // if symbol is an operand
+    else {
+ 
+      // push the operand to the stack
+      s.push(string(1, pre_exp[i]));
     }
-};
-
-TreeNode *createTree(string str) {
-    if (str.length() == 0 || str[0] == 'N') {
-        return NULL;
-    }
-
-    vector<string> v;
-
-    istringstream iss(str);
-    for (string str; iss >> str; ) {
-        v.push_back(str);
-    }
-
-    TreeNode *root = new TreeNode(stoi(v[0]));
-
-    int i = 1;
-
-    queue<TreeNode *> q;
-    q.push(root);
-
-    while (!q.empty() && i < v.size()) {
-        TreeNode *curr = q.front();
-        q.pop();
-
-        string currVal = v[i];
-
-        //Implementing left child
-        if (currVal != "N") {
-            curr->left = new TreeNode(stoi(currVal));
-            q.push(curr->left);
-        }
-
-        ++i;
-
-        if (i >= v.size()) break;
-        
-        currVal = v[i];
-
-        //Implementing right child
-        if (currVal != "N") {
-            curr->right = new TreeNode(stoi(currVal));
-            q.push(curr->right);
-        }
-
-        ++i;
-    }
-
-    return root;
+  }
+ 
+  // Stack now contains the Infix expression
+  return s.top();
 }
 
-class Solution {
-public:
-    void sumNode(TreeNode* root) {
-        if (root == nullptr) return;
-        if (root->left == nullptr && root->right == nullptr) return;
-        
-        if (root->left == nullptr) {
-            root->right->val += root->val;
-            sumNode(root->right);
-            return;
-        }
-        if (root->right == nullptr) {
-            root->left->val += root->val;
-            sumNode(root->left);
-            return;
-        }
-        
-        root->right->val += root->val;
-        root->left->val += root->val;
-        sumNode(root->right);
-        sumNode(root->left);
-    }
-    void traverse(TreeNode* root, int targetSum, bool& result) {
-        if (root == nullptr) return;
-        
-        traverse(root->left, targetSum, result);
-        if (root->left == nullptr && root->right == nullptr) {
-            if (root->val == targetSum) result = true;
-            return;
-        }
-        traverse(root->right, targetSum, result);
-    }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if (root == nullptr) return false;
-        sumNode(root);
-        
-        bool result;
-        traverse(root, targetSum, result);
+int main() {
+	string a = "*+-/abc*+def*gh";
+    cout << a.length() << endl;
+	string result = preToInfix(a);
 
-        return result;
-    }
-};
-
-void print(TreeNode *root) {
-    if (root == NULL) {
-        cout << "Tree is empty!\n";
-        return;
-    }
-
-    queue<TreeNode*> q;
-    q.push(root);
-
-    while (!q.empty()) {
-        TreeNode *curr = q.front();
-        q.pop();
-
-        if (curr == NULL) {
-            cout << "N ";
-        }
-        else {
-            cout << curr->val << " ";
-            q.push(curr->left);
-            q.push(curr->right);
-        }
-    }
-    cout << endl;
-}
-
-int main() {  
-    TreeNode* root = nullptr;
-    int a = 22;
-
-    string str = "5 4 8 11 N 13 4 7 2 N N N 1";
-
-    root = createTree(str);
-
-    print(root);
-
-    Solution obj;
-
-    cout << obj.hasPathSum(root, a) << endl;
-
-    print(root);
-    return 0;
+    cout << result;
+	return 0;
 }
