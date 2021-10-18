030f328e8df1e6bb260e8ffec840b1ed1b0a3aac
Daniel Nguyễn <danielnguyen2701@gmail.com>
2021-10-18 14:33:10 +0700

    commit by vscode

--- Testing/test2.cpp
+++ Testing/test2.cpp
@@ -4,14 +4,5 @@
 using namespace std;
 
 int main() {
-
-    double a = 0.5;
-
-    for (long long i = 2; i <= 1000000000; i++)
-    {
-        a *= (2 * i - 1) / (double)(2 * i);
-    }
-
-    cout << setprecision(10) << a << endl;
-    return 0;
+    int d = 
 }
