df575ac77fc92a1af05229e22cf1a83f6e1512a6
Daniel Nguyễn <danielnguyen2701@gmail.com>
2021-10-29 20:44:18 +0700

    main

--- Testing/test.cpp
+++ Testing/test.cpp
@@ -2,53 +2,10 @@
 
 using namespace std;
 
-string highAndLow(const string& numbers)
-{
-  //your code here
-  string temp;
-  string max; 
-  string min;
-  
-  int i = 0;
-  while (numbers[i] != ' ')
-  {
-    i++;  
-  }
-  
-  max = numbers.substr(0, i);
-  min = numbers.substr(0, i);
-  
-  i++;
-  for (; i < (int)numbers.length(); i++)
-  {
-    int j = i;
-    while (numbers[i] && numbers[i] != ' ') 
-    {
-      i++;
-    }
-    
-    temp = numbers.substr(j, i - j);
-    if (stoi(temp) > stoi(max)) 
-    {
-      max.clear();
-      max = temp;
-    }
-    if (stoi(temp) < stoi(min))
-    {
-      min.clear();
-      min = temp;
-    }
-    
-    temp.clear();
-  }
-  
-  return max + " " + min;
-}
 int main()
 {
-    string input = "8 3 -5 42 -1 0 0 -9 4 7 4 -4";
-
-    cout << highAndLow(input);
+    int x = 0x72;
+    cout << x << endl;
 
     return 0;
 }
