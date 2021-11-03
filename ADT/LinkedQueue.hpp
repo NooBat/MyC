4a07fdfc50f2723825bfdd970d43b157359f4e8a
Daniel Nguyễn <danielnguyen2701@gmail.com>
2021-11-03 10:55:55 +0700

    implementing linked queue

---
+++ ADT/LinkedQueue.hpp
@@ -0,0 +1,63 @@
+#ifndef _LINKED_QUEUE
+#define _LINKED_QUEUE
+
+#include "QueueInterface.hpp"
+#include "PrecondViolatedException.hpp"
+#include "Node.hpp"
+
+using namespace std;
+
+template<class ItemType>
+class LinkedQueue : public QueueInterface<ItemType>
+{
+private:
+    Node<ItemType>* headPtr;
+    Node<ItemType>* tailPtr;
+
+public:
+    LinkedQueue();
+    LinkedQueue(const LinkedQueue<ItemType>& other);
+    virtual ~LinkedQueue();
+
+    bool isEmpty() const;
+    bool enqueue(const ItemType& newEntry);
+    bool dequeue();
+    ItemType peekFront() const;
+};
+
+template<class ItemType>
+LinkedQueue<ItemType>::LinkedQueue()
+{
+    headPtr = nullptr;
+    tailPtr = nullptr;
+}
+
+template<class ItemType>
+LinkedQueue<ItemType>::LinkedQueue(const LinkedQueue<ItemType>& other)
+{
+    headPtr = nullptr;
+    tailPtr = nullptr;
+    if (!other.isEmpty())
+    {
+        Node<ItemType>* currPtr = other.headPtr;
+        headPtr = new Node<ItemType>(currPtr->getItem());
+        tailPtr = headPtr;
+        currPtr = currPtr->getNext();
+
+        while (currPtr)
+        {
+            Node<ItemType>* newNodePtr = new Node<ItemType>(currPtr->getItem());
+            tailPtr->setNext(newNodePtr);
+            currPtr = currPtr->getNext();
+            tailPtr = tailPtr->getNext();
+        }
+    }
+}
+
+template<class ItemType>
+LinkedQueue<ItemType>::~LinkedQueue()
+{
+    
+}
+
+#endif
--- Testing/test.cpp
+++ Testing/test.cpp
@@ -2,28 +2,15 @@
 
 using namespace std;
 
-enum BalanceValue
-{
-    LH = -1,
-    EH = 0,
-    RH = 1
-};
-
-class Node
-{
-public:
-    int data;
-    Node* next;
-    BalanceValue balance;
-
-    Node(int data): data(data), next(nullptr), balance(EH) {}
-    ~Node() {}
-};
-
 int main()
 {  
-    Node* head = new Node(2);
+    int n = 5;
+    do
+    {
+        cout << n << " ";
+        n++;
+    }
+    while (n < 5);
 
-    cout << head->balance << endl;
     return 0;
 }
