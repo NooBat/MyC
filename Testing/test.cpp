b9e75f829bb4ad0dea3b61cbd230c3a2600f3be7
Daniel Nguyễn <danielnguyen2701@gmail.com>
2021-11-04 21:00:09 +0700

    main

--- Testing/test.cpp
+++ Testing/test.cpp
@@ -4,13 +4,34 @@ using namespace std;
 
 int main()
 {  
-    int n = 5;
-    do
+    int n;
+    cin >> n;
+    int arr[n];
+    bool checked[n];
+
+    for (int i = 0; i < n; i++)
     {
-        cout << n << " ";
-        n++;
+        cin >> arr[i];
+        checked[i] = false;
     }
-    while (n < 5);
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        if (!checked[i])
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (arr[j] == arr[i])
+                {
+                    checked[j] = true;
+                    count++;
+                }
+            }
+
+            cout << "Frequency of " << arr[i] << ": " << count << endl;
+        }
+    }
 
     return 0;
 }
