fe16d5a9189b5d0284259c61aad908f4729c3da3
Daniel Nguyễn <danielnguyen2701@gmail.com>
2021-11-04 14:41:45 +0700

    commit by vscode

--- Testing/test.cpp
+++ Testing/test.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int main()
 {  
-    int n = 5;
-    do
-    {
-        cout << n << " ";
-        n++;
-    }
-    while (n < 5);
-
-    return 0;
+    int n;
+    cin >> n;
+    
 }
