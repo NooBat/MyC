ba62122627aad8d654d4d338f65090652cfd7bcd
NooBat <danielnguyen2701@gmail.com>
2021-12-09 15:23:32 +0700

    book list implementation

--- Testing/test.cpp
+++ Testing/test.cpp
@@ -20,37 +20,37 @@ public:
     int numberOfBooks;
 };
 
-bool inputData(BookList bookList, string title, string author[], string year)
+void printBook(Book& book);
+void printBookList(BookList& bookList);
+bool findBook(BookList& bookList, string title);
+void editData(Book& book, string title, string author[], int numberOfAuthor, string year);
+
+bool inputData(BookList& bookList, string title, string author[], int numberOfAuthor, string year)
 {
     if (bookList.numberOfBooks == MAX_NUMBER_OF_BOOKS) return false;
-    int numberOfAuthor = (int)(sizeof(author) / sizeof(author[0]));
     if (numberOfAuthor > 30) return false;
+
     Book newBook;
-    newBook.title = title;
-    
-    for (int i = 0; i < numberOfAuthor; i++)
-    {
-        newBook.author[i] = author[i];
-    }
-    newBook.numberOfAuthor = numberOfAuthor;
-    newBook.year = year;
+
+    editData(newBook, title, author, numberOfAuthor, year);
 
     bookList.list[bookList.numberOfBooks++] = newBook; 
 
     return true;
 }
 
-void printBook(Book book)
+void printBook(Book& book)
 {
-    cout << setw(100) << left << "Title" << setw(50) << left << "Author" << "Year" << endl;
-    cout << setw(100) << left << book.title << setw(50) << left << book.author[0] << book.year << endl;
+    cout << setw(30) << left << "Title" << setw(30) << left << "Author" << "Year" << endl;
+    cout << setw(30) << left << book.title << setw(30) << left << book.author[0] << book.year << endl;
     for (int i = 1; i < book.numberOfAuthor; i++) 
     {
-        cout << setw(150) << right << book.author[i] << endl;
+        cout << setw(30) << "" << setw(30) << left << book.author[i] << endl;
     }
+    cout << endl;
 }
 
-void printBookList(BookList bookList)
+void printBookList(BookList& bookList)
 {
     for (int i = 0; i < bookList.numberOfBooks; i++)
     {
@@ -58,22 +58,64 @@ void printBookList(BookList bookList)
     }
 }
 
+bool findBook(BookList& bookList, string title) 
+{
+    for (int i = 0; i < bookList.numberOfBooks; i++) 
+    {
+        if (title == bookList.list[i].title) return true;
+    }
+
+    return false;
+}
+
+void editData(Book& book, string title, string author[], int numberOfAuthor, string year)
+{
+    book.title = title;
+    for (int i = 0; i < numberOfAuthor; i++)
+    {
+        book.author[i] = author[i];
+    }
+    book.numberOfAuthor = numberOfAuthor;
+    book.year = year;
+    book.numberOfAuthor = numberOfAuthor;
+}
+
 int main()
 {  
     BookList bookList;
     bookList.numberOfBooks = 0;
-    for (int i = 0; i < 10; i++)
+    int numberOfBooks = 0;
+    cout << "How many books: ";
+    cin >> numberOfBooks;
+
+    for (int i = 0; i < numberOfBooks; i++)
     {
-        string author[10];
-        for (int j = 0; j < 10; j++)
+        cin.ignore();
+        string title;
+        cout << "Input the title of the book " + to_string(i + 1) << ": ";
+        getline(cin, title);
+
+        int numberOfAuthor;
+        cout << "Number of Author: ";
+        cin >> numberOfAuthor;
+        
+        cin.ignore(1000, '\n');
+        string author[numberOfAuthor];
+        for (int j = 0; j < numberOfAuthor; j++)
         {
-            author[j] = "ABDG";
+            cout << "Name of author " + to_string(j + 1) << ": ";
+            getline(cin, author[j]);
         }
 
-        inputData(bookList, "Book " + to_string(i + 1), author, "1999");
+        int year;
+        cout << "Year: ";
+        cin >> year;
+
+        inputData(bookList, title, author, numberOfAuthor, to_string(year));
     }
-    cout << "a" << endl;
     printBookList(bookList);
+
+    findBook(bookList, "Book 1");
 
     return 0;
 }
