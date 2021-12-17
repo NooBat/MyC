ea56d8fe937368cc0f17bc48d5f6fbcba0af7f77
NooBat <danielnguyen2701@gmail.com>
2021-12-18 03:08:47 +0700

    commit by vscode

---
+++ ADT/Iterator/LinkedIterator.hpp
@@ -0,0 +1,27 @@
+#ifndef _LINKED_ITERATOR      
+#define _LINKED_ITERATOR
+
+#include<iterator>
+#include "../Node.hpp"
+
+template<class T> 
+class LinkedList;
+
+template<class T>
+class LinkedIterator : public iterator<input_iterator_tag, int>
+{
+private:
+    // ADT associated with iterator
+    const LinkedList<ItemType>* containerPtr;
+
+    //Current location in collection
+    Node<T>* currentItemPtr;
+
+public:
+    LinkedIterator(const LinkedList<T>* someList, Node<T>* nodePtr);
+
+    /**Dereferencing operator overload.
+     * @return  **/ 
+};
+
+#endif
