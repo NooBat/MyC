4d34bc2d26bb2a5cf9faa39fe25bd50cdc233e92
NooBat <danielnguyen2701@gmail.com>
2022-02-27 10:23:06 +0700

    commit by vscode

--- Assignment1-212/studyInPink2.h
+++ Assignment1-212/studyInPink2.h
@@ -26,6 +26,84 @@ using namespace std;
 ///DO NOT modify any parameters in the functions.
 ////////////////////////////////////////////////////////////////////////
 
+//Helper function for function notebook2
+int countOccurences(string str, string s) {
+    if (str.length() < s.length()) return 0;
+    int result = 0;
+    int idx = 0;
+
+    while (idx != -1) {
+        idx = str.find(s);
+        if (idx != -1) {
+            result += 1;
+            str.replace(idx, s.length(), "");        
+        }
+    }
+
+    return result;
+}
+
+//Helper functions for function notebook3
+bool isPrime(int n) {
+    if (n < 2) return false;
+    if (n < 4) return true;
+
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
+
+bool isSquare(int n) {
+    int squareRoot = sqrt(n);
+
+    return (squareRoot * squareRoot == n);
+}
+
+bool isFibonacci(int n) {
+    return (isSquare(5 * n * n - 4) && isSquare(5 * n * n + 4));
+}
+
+//Helper function for function generateListPasswords
+int indexOfMax(int *arr) {
+    int max = arr[0];
+    int index = 0;
+
+    for (int i = 1; i < 10; i++) {
+        if (arr[i] >= max) {
+            max = arr[i];
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+
+string functionG(string s1, string s2) {
+    int temp = 0;
+    string result;
+
+    for (int i = 0; i < s1.length(); i++) {
+        int digit1 = stoi(s1.substr(i, 1));
+        int digit2 = stoi(s2.substr(i, 1));
+
+        int sum = digit1 + digit2 + temp;
+
+        if (sum > 9) {
+            sum %= 10;
+            temp = 1;
+        } else {
+            temp = 0;
+        }
+
+        result += to_string(sum);
+    }
+
+    return result;
+}
+
 string notebook1(string ntb1) {
     // Complete this function to gain point
     ifstream file;
@@ -77,8 +155,9 @@ string notebook1(string ntb1) {
 string notebook2(string ntb2) {
     // Complete this function to gain point
     ifstream file;
-    file.open(ntb1);
+    file.open(ntb2);
 
+    string result;
     string line;
     int n2;
 
@@ -100,23 +179,94 @@ string notebook2(string ntb2) {
         return "1111111111";
     }
 
+    int pinkCount = 0;
+
     for (int i = 0; i < n2; i++) {
         getline(file, line);
-
-        
+        pinkCount += countOccurences(line, "pink") + countOccurences(line, "Pink");
     }
 
-    return "000000000";
+    if (pinkCount < 10000) {
+        pinkCount *= pinkCount;
+    }   
+    
+    result += to_string(pinkCount);
+
+    while (result.length() < 10) {
+        result += to_string(9);
+    }
+
+    return result;
 }
 
 string notebook3(string ntb3) {
     // Complete this function to gain point
-    return "000000000";
+    int arr[10][10];
+
+    ifstream file;
+    file.open(ntb3);
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            int temp;
+            char line;
+
+            file >> temp;
+            
+            arr[i][j] = abs(temp);
+
+            if (j < 9) file >> line;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        while (!isPrime(arr[i][i])) {
+            arr[i][i]++;
+        }
+    }
+
+    for (int i = 1; i < 10; i++) {
+        for (int j = 0; j < i; j++) {
+            while (!isFibonacci(arr[i][j])) {
+                arr[i][j]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (arr[i][7] > arr[i][8]) swap(arr[i][7], arr[i][8]);
+        if (arr[i][8] > arr[i][9]) swap(arr[i][8], arr[i][9]);
+        if (arr[i][7] > arr[i][9]) swap(arr[i][7], arr[i][9]);
+    }
+
+    string pwd;
+
+    for (int i = 0; i < 10; i++) {
+        pwd += to_string(indexOfMax(arr[i]));
+    }
+
+    return pwd;
 }
 
 string generateListPasswords(string pwd1, string pwd2, string pwd3) {
     // Complete this function to gain point
-    return "000000000";
+    string passwordsList = "";
+
+    passwordsList += pwd1 + "," + pwd2 + "," + pwd3 + ",";
+
+    passwordsList += functionG(pwd1, pwd2) + ",";
+
+    passwordsList += functionG(pwd1, pwd3) + ",";
+
+    passwordsList += functionG(pwd2, pwd3) + ",";
+
+    passwordsList += functionG(functionG(pwd1, pwd2), pwd3) + ",";
+
+    passwordsList += functionG(pwd1, functionG(pwd2, pwd3)) + ",";
+
+    passwordsList += functionG(functionG(pwd1, pwd2), functionG(pwd1, pwd3));
+
+    return passwordsList;
 }
 
 bool chaseTaxi(
